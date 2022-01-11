f71c87c6eb575d34e4b6e176c5b2c9296ab85fd3
qiuhaidong <690982322@qq.com>
2022-01-12 02:07:51 +0800

    update

--- push.sh
+++ push.sh
@@ -2,6 +2,5 @@
 cd /home/dong/octopress
 #rake generate deploy
 git add .
-#git commit -m 'update'
-git commit -m 'ubuntu20.04 octopress2.0'
+git commit -m 'update'
 git push origin source
---
+++ source/_posts/2022-01-12-write-them-all.markdown
@@ -0,0 +1,8 @@
+---
+layout: post
+title: "好记性不如烂笔头"
+date: 2022-01-12 00:37:26 +0800
+comments: true
+categories: personal
+---
+很多以前做过的事，细节步骤都不记得了，多写字吧，详细地记录下来，以后也有个回忆。

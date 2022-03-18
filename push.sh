71063746faa3f6e328ed9fdf30cd86597581c629
qiuhaidong <690982322@qq.com>
2022-03-18 16:42:34 +0800

    update

--- push.sh
+++ push.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-sudo mv /home/dong/.ssh2 /home/dong/.ssh
+# sudo mv /home/dong/.ssh2 /home/dong/.ssh
 cd /home/dong/octopress
 rake generate deploy
 git add .
 git commit -m 'update'
 git push origin source
-sudo mv /home/dong/.ssh /home/dong/.ssh2
+# sudo mv /home/dong/.ssh /home/dong/.ssh2

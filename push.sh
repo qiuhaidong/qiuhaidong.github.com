1c1a68429ebaba5f2b6357a97bd8c26b88317ad2
qiuhaidong <690982322@qq.com>
2022-04-01 21:43:22 +0800

    blog

--- push.sh
+++ push.sh
@@ -3,6 +3,6 @@ mv /home/dong/.ssh2 /home/dong/.ssh
 cd /home/dong/octopress
 rake generate deploy
 git add .
-git commit -m 'update'
+git commit -m 'blog'
 git push origin source
 mv /home/dong/.ssh /home/dong/.ssh2

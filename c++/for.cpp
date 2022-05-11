cd0f65bd66638570520309cf69aceedf10e5be14
qiuhaidong <690982322@qq.com>
2022-05-11 16:55:45 +0800

    blog

--- c++/for.cpp
+++ c++/for.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
 	int a;
-	for (a=0;a<100;a++)
+	for (a=0;a<300;a++)
 		{
 			cout<<a<<endl;
 		}
---
+++ c/for.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	int a;
+	for (a=0;a<300;a=a+1)
+	{
+		printf("%d\n",a);
+	}
+	return 0;
+}
